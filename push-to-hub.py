6b05e4eb068c2f8d1a4b28a35ce6ed66290ac1c0
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-09-09 15:19:26 -0400

    that file wasn't supposed to be there

--- push-to-hub.py
+++
@@ -1,5 +0,0 @@
-from datasets import load_dataset
-
-dataset = load_dataset("imagefolder", data_dir="repo")
-print(dataset["train"][0])
-#dataset.push_to_hub(input("Enter which repo you'd like to push this to: "))
